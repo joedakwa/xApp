9161b13216a8f65a540f84e008dd87c74691c33d
joe dakwa <joedakwa@hotmail.co.uk>
2023-05-22 14:44:04 +0100

    add connect_wallet

---
+++ simple-xrpl-wallet/src/assets/connect-wallet/connect-wallet.js
@@ -0,0 +1,7 @@
+import { Client, Wallet, dropsToXrp, isValidClassicAddress, xrpToDrops } from 'xrpl';
+
+import getWalletDetails from '../helpers/get-wallet-details';
+import renderXrplLogo from '../helpers/render-xrpl-logo';
+
+renderXrplLogo();
+
